e68cb7927f902024ffca843004ef20808b1fa18a
trombini <leandro.ae@gmail.com>
2023-04-09 14:57:09 -0300

    second commit

---
+++ Estruturas de dados/01 - Listas/00 _declarando_listas.py
@@ -0,0 +1,17 @@
+frutas = ["laranja", "maça", "uva"]
+print(frutas)
+print(frutas[1])
+print(frutas[-1])
+
+frutas = []
+print(frutas)
+
+letras = list("python")
+print(letras)
+print(letras[4])
+
+numeros = list(range(10))
+print(numeros)
+
+carro = ["Ferrari", "F8", 4200000, 2020, 2090, "São Paulo", True]
+print(carro)
---
+++ Estruturas de dados/03 - Conjuntos/01_acessando_dados.py
@@ -0,0 +1,5 @@
+numeros = {1, 2, 3, 2} # Para acessa um indice de um set tem que transforma lo em lista
+
+numeros = list(numeros)
+
+print(numeros[0])
